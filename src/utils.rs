97dc857c486731d9fa0f5f8b0388dbf0815047d8
QingGo <zyqingjohn@qq.com>
2021-11-13 03:38:09 +0800

    fail if engine changed, and pass all test

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,9 @@
 use anyhow::anyhow;
 use anyhow::Result;
 use slog::Drain;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 pub fn get_root_logger(process: String) -> slog::Logger {
     let decorator = slog_term::TermDecorator::new().stderr().build();
@@ -10,25 +13,40 @@ pub fn get_root_logger(process: String) -> slog::Logger {
 }
 
 pub fn get_last_engine() -> Option<String> {
-    Some("kvs".to_string())
+    if let Ok(file) = File::open("last_engine.txt") {
+        let mut file = BufReader::new(file);
+        let mut line = String::new();
+        file.read_line(&mut line).unwrap();
+        return Some(line.trim().to_string());
+    } else {
+        None
+    }
+}
+
+pub fn write_engine_to_file(engine: &str) -> Result<()> {
+    let mut file = File::create("last_engine.txt")?;
+    file.write_all(engine.as_bytes())?;
+    Ok(())
 }
 
 pub fn get_engine(last_engine: Option<String>, config_engine: Option<String>) -> Result<String> {
     let engine: String;
     if let Some(last_engine) = last_engine {
         if let Some(config_engine) = config_engine {
-            // if config_engine != last_engine {
-            //     return Err(anyhow!("engine not match"));
-            // } else {
-            //     engine = config_engine;
-            // }
-            engine = config_engine;
+            if config_engine != last_engine {
+                return Err(anyhow!("engine not match"));
+            } else {
+                engine = config_engine;
+            }
         } else {
             engine = last_engine;
         }
+    } else if let Some(config_engine) = config_engine {
+        engine = config_engine;
     } else {
         engine = "kvs".to_string();
     }
+    write_engine_to_file(&engine)?;
     Ok(engine)
 }
 
