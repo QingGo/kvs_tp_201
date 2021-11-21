8b23dda1ac1a90e066928c67b6448c5d418efda1
QingGo <zyqingjohn@qq.com>
2021-11-21 17:44:30 +0800

    use crossbeam_channel for shared_queue thread pool implement cuz efficient communication

--- src/thread_pool/shared_queue.rs
+++ src/thread_pool/shared_queue.rs
@@ -1,71 +1,59 @@
 use anyhow::Result;
-use std::collections::VecDeque;
-use std::sync::Mutex;
-use std::sync::atomic::{AtomicBool, Ordering};
-use std::{
-    sync::Arc,
-    thread::{self, sleep, JoinHandle},
-    time::Duration,
-};
+use crossbeam_channel::{bounded, Sender};
+use std::thread::{self, JoinHandle};
 
 use super::ThreadPool;
 
-type ShardQueue = Arc<Mutex<VecDeque<Box<dyn FnOnce() + Send + 'static>>>>;
-
+enum ThreadPoolMessage {
+    RunJob(Box<dyn FnOnce() + Send + 'static>),
+    Shutdown,
+}
 #[allow(unused)]
 pub struct SharedQueueThreadPool {
     workers: Vec<JoinHandle<()>>,
-    queue: ShardQueue,
-    is_finish: Arc<AtomicBool>,
+    /// use crossbeam_channel for efficient communication
+    sender: Sender<ThreadPoolMessage>,
 }
 
 impl ThreadPool for SharedQueueThreadPool {
     fn new(threads: u32) -> Result<Self> {
-        let queue: ShardQueue =
-            Arc::new(Mutex::new(VecDeque::new()));
+        let (sender, receiver) = bounded::<ThreadPoolMessage>(100);
         let mut workers = Vec::with_capacity(threads as usize);
-        let is_finish = Arc::new(AtomicBool::new(false));
         for _ in 0..threads {
-            let queue_clone = Arc::clone(&queue);
-            let is_finish_clone = Arc::clone(&is_finish);
-            let worker = thread::spawn(move || loop {
-                // the scope of the temporary value in the if let condition is the whole if/let construct
-                let task_option = queue_clone.lock().unwrap().pop_front();
-                if let Some(task) = task_option {
-                    task();
-                } else {
-                    if is_finish_clone.load(Ordering::SeqCst) {
-                        break;
+            let receiver_clone = receiver.clone();
+            let worker = thread::spawn(move || {
+                while let Ok(task) = receiver_clone.recv() {
+                    // the scope of the temporary value in the if let condition is the whole if/let construct
+                    match task {
+                        ThreadPoolMessage::RunJob(job) => {
+                            job();
+                        }
+                        ThreadPoolMessage::Shutdown => {
+                            break;
+                        }
                     }
-                    sleep(Duration::from_millis(100));
                 }
             });
             workers.push(worker);
         }
-        Ok(Self {
-            workers,
-            queue,
-            is_finish,
-        })
+        Ok(Self { workers, sender })
     }
 
     fn spawn<F>(&self, job: F)
     where
         F: FnOnce() + Send + 'static,
     {
-        self.queue.lock().unwrap().push_back(Box::new(job));
+        self.sender
+            .send(ThreadPoolMessage::RunJob(Box::new(job)))
+            .unwrap();
     }
 }
 
-impl SharedQueueThreadPool{
+impl SharedQueueThreadPool {
     #[allow(unused)]
     fn join(self) -> Result<()> {
-        loop {
-            if self.queue.lock().unwrap().len() == 0 {
-                self.is_finish.store(true, Ordering::SeqCst);
-                break;
-            }
-            sleep(Duration::from_millis(100));
+        for i in 0..self.workers.len() {
+            self.sender.send(ThreadPoolMessage::Shutdown).unwrap();
         }
         for worker in self.workers {
             worker.join().unwrap();
@@ -76,6 +64,8 @@ impl SharedQueueThreadPool{
 
 #[test]
 fn test() {
+    use std::thread::sleep;
+    use std::time::Duration;
     let pool = SharedQueueThreadPool::new(4).unwrap();
     for i in 0..10 {
         pool.spawn(move || {
