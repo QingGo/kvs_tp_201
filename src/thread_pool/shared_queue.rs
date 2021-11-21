2059a8ae8989eba689ce842ceb46d32fec5adae2
QingGo <zyqingjohn@qq.com>
2021-11-21 19:04:04 +0800

    1. use drop trait and thread::panicking to detect and respawn a new thread to run task. 2. use crossbeam_utils::sync::WaitGroup to waiting thread which is unfinished is test.

--- src/thread_pool/shared_queue.rs
+++ src/thread_pool/shared_queue.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use crossbeam_channel::{bounded, Sender};
-use std::thread::{self, JoinHandle};
+use std::thread;
 
 use super::ThreadPool;
 
@@ -8,35 +8,43 @@ enum ThreadPoolMessage {
     RunJob(Box<dyn FnOnce() + Send + 'static>),
     Shutdown,
 }
+
 #[allow(unused)]
 pub struct SharedQueueThreadPool {
-    workers: Vec<JoinHandle<()>>,
+    num_threads: u32,
     /// use crossbeam_channel for efficient communication
     sender: Sender<ThreadPoolMessage>,
+}
+
+/// wrap crossbeam_channel::Receiver to imply drop trait
+#[derive(Clone)]
+struct TaskReceiver {
+    receiver: crossbeam_channel::Receiver<ThreadPoolMessage>,
+}
+
+impl Drop for TaskReceiver {
+    fn drop(&mut self) {
+        // if thread end because of panic, then open a new thread run the task with the cloned receiver
+        if thread::panicking() {
+            let receiver = self.clone();
+            thread::spawn(move || run_task(receiver));
+        }
+    }
 }
 
 impl ThreadPool for SharedQueueThreadPool {
     fn new(threads: u32) -> Result<Self> {
         let (sender, receiver) = bounded::<ThreadPoolMessage>(100);
-        let mut workers = Vec::with_capacity(threads as usize);
         for _ in 0..threads {
-            let receiver_clone = receiver.clone();
-            let worker = thread::spawn(move || {
-                while let Ok(task) = receiver_clone.recv() {
-                    // the scope of the temporary value in the if let condition is the whole if/let construct
-                    match task {
-                        ThreadPoolMessage::RunJob(job) => {
-                            job();
-                        }
-                        ThreadPoolMessage::Shutdown => {
-                            break;
-                        }
-                    }
-                }
-            });
-            workers.push(worker);
+            let task_receiver = TaskReceiver {
+                receiver: receiver.clone(),
+            };
+            thread::spawn(move || run_task(task_receiver));
         }
-        Ok(Self { workers, sender })
+        Ok(Self {
+            num_threads: threads,
+            sender,
+        })
     }
 
     fn spawn<F>(&self, job: F)
@@ -52,26 +60,41 @@ impl ThreadPool for SharedQueueThreadPool {
 impl SharedQueueThreadPool {
     #[allow(unused)]
     fn join(self) -> Result<()> {
-        for i in 0..self.workers.len() {
+        for i in 0..self.num_threads {
             self.sender.send(ThreadPoolMessage::Shutdown).unwrap();
-        }
-        for worker in self.workers {
-            worker.join().unwrap();
         }
         Ok(())
     }
 }
 
+fn run_task(task_receiver: TaskReceiver) {
+    while let Ok(task) = task_receiver.receiver.recv() {
+        // the scope of the temporary value in the if let condition is the whole if/let construct
+        match task {
+            ThreadPoolMessage::RunJob(job) => {
+                job();
+            }
+            ThreadPoolMessage::Shutdown => {
+                break;
+            }
+        }
+    }
+}
+
 #[test]
 fn test() {
+    use crossbeam_utils::sync::WaitGroup;
     use std::thread::sleep;
     use std::time::Duration;
     let pool = SharedQueueThreadPool::new(4).unwrap();
+    let wg = WaitGroup::new();
     for i in 0..10 {
+        let wg = wg.clone();
         pool.spawn(move || {
             println!("Hello from thread {}!", i);
             sleep(Duration::from_millis(2000));
+            drop(wg);
         });
     }
-    pool.join().unwrap();
+    wg.wait();
 }
